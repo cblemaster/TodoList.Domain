b900f586a0a9f5be929890779382e27424694eb7
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-03 18:24:07 -0500

    added todo list entity model

---
+++ TodoList.cs
@@ -0,0 +1,30 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Domain;
+
+public class TodoList : List<Todo>
+{
+    private const int MAX_LENGTH_FOR_NAME = 255;
+    private const int MIN_LENGTH_FOR_NAME = 1;
+
+    public Guid TodoListId { get; set; }
+    public string Name { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+
+    private TodoList(string name)
+    {
+        TodoListId = Guid.NewGuid();
+        Name = name;
+        CreateDate = DateTime.Now;
+    }
+    
+    public static TodoList Create([Required, MaxLength(MAX_LENGTH_FOR_NAME), MinLength(MIN_LENGTH_FOR_NAME)] string name) => new(name);
+    public void Rename([Required, MaxLength(MAX_LENGTH_FOR_NAME)] string name)
+    {
+        Name = name;
+        UpdateDate = DateTime.Now;
+    }
+    public bool IsValidForDelete() => this.All(t => t.IsComplete);
+}
