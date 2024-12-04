debdd5215bddb67066b328a6a1d1a0967fbc613f
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-03 20:25:49 -0500

    made all primitives record structs

--- Primitives/TodoDescription.cs
+++ Primitives/TodoDescription.cs
@@ -3,7 +3,4 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TodoList.Domain.Primitives;
 
-public class TodoDescription([Required, MaxLength(Constants.MAX_LENGTH_FOR_TODO_DESC), MinLength(Constants.MIN_LENGTH_FOR_TODO_DESC)] string value)
-{
-    public string Value { get; set; } = value;
-}
+public readonly record struct TodoDescription([Required, MaxLength(Constants.MAX_LENGTH_FOR_TODO_DESC), MinLength(Constants.MIN_LENGTH_FOR_TODO_DESC)] string Value);
--- Primitives/TodoListName.cs
+++ Primitives/TodoListName.cs
@@ -3,7 +3,4 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TodoList.Domain.Primitives;
 
-public class TodoListName([Required, MaxLength(Constants.MAX_LENGTH_FOR_TODOLIST_NAME), MinLength(Constants.MIN_LENGTH_FOR_TODOLIST_NAME)] string value)
-{
-    public string Value { get; set; } = value;
-}
+public readonly record struct TodoListName([Required, MaxLength(Constants.MAX_LENGTH_FOR_TODOLIST_NAME), MinLength(Constants.MIN_LENGTH_FOR_TODOLIST_NAME)] string Value);
