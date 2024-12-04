528318364c2accc7c8dbf6b76546db719915424d
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-03 20:00:32 -0500

    added primitives to encapsulate list name and todo description

---
+++ Primitives/TodoDescription.cs
@@ -0,0 +1,9 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Domain.Primitives;
+
+public class TodoDescription([Required, MaxLength(Constants.MAX_LENGTH_FOR_TODO_DESC), MinLength(Constants.MIN_LENGTH_FOR_TODO_DESC)] string value)
+{
+    public string Value { get; set; } = value;
+}
---
+++ Primitives/TodoListName.cs
@@ -0,0 +1,9 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Domain.Primitives;
+
+public class TodoListName([Required, MaxLength(Constants.MAX_LENGTH_FOR_TODOLIST_NAME), MinLength(Constants.MIN_LENGTH_FOR_TODOLIST_NAME)] string value)
+{
+    public string Value { get; set; } = value;
+}
