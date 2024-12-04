84106ed9dd87fdec77969e7c528534bfc33a9317
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-03 19:58:58 -0500

    added static constants class

---
+++ Primitives/Constants.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TodoList.Domain.Primitives;
+
+internal static class Constants
+{
+    internal const int MAX_LENGTH_FOR_TODOLIST_NAME = 255;
+    internal const int MIN_LENGTH_FOR_TODOLIST_NAME = 1;
+
+    internal const int MAX_LENGTH_FOR_TODO_DESC = 255;
+    internal const int MIN_LENGTH_FOR_TODO_DESC = 1;
+}
