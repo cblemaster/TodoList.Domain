f05b07589d174988e50befd2a2060a448f2b841c
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-03 20:01:05 -0500

    added entity classes

---
+++ Entities/Todo.cs
@@ -0,0 +1,36 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using TodoList.Domain.Primitives;
+
+namespace TodoList.Domain.Entities;
+
+public class Todo
+{
+    public TodoId TodoId { get; set; }
+    public TodoListId TodoListId { get; set; }
+    public TodoDescription Description { get; set; }
+    public DateOnly? DueDate { get; set; }
+    public bool IsImportant { get; set; }
+    public bool IsComplete { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+
+    private Todo(TodoListId todoListId, string description, DateOnly? dueDate, bool isImportant, bool isComplete)
+    {
+        TodoId = new(Guid.CreateVersion7());
+        TodoListId = todoListId;
+        Description = new(description);
+        DueDate = dueDate;
+        IsImportant = isImportant;
+        IsComplete = isComplete;
+        CreateDate = DateTime.Now;
+    }
+
+    public bool IsIncludeInDueToday() => DueDate.HasValue && DueDate.Value == DateOnly.FromDateTime(DateTime.Today) && !IsComplete;
+    public bool IsIncludeInImportant() => IsImportant && !IsComplete;
+    public bool IsIncludeInCompleted() => IsComplete;
+
+    public static Todo Create(TodoListId todoListId, [Required, MaxLength(Constants.MAX_LENGTH_FOR_TODO_DESC), MinLength(Constants.MIN_LENGTH_FOR_TODO_DESC)] string description, DateOnly? dueDate, bool isImportant, bool isComplete) => new(todoListId, description, dueDate, isImportant, isComplete);
+
+    public bool IsValidForDelete() => !IsImportant;
+}
---
+++ Entities/TodoList.cs
@@ -0,0 +1,31 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using TodoList.Domain.Primitives;
+
+namespace TodoList.Domain.Entities;
+
+public class TodoList : List<Todo>
+{
+    private const int MAX_LENGTH_FOR_NAME = 255;
+    private const int MIN_LENGTH_FOR_NAME = 1;
+
+    public TodoListId Id { get; set; }
+    public TodoListName Name { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+
+    private TodoList(string name)
+    {
+        Id = new(Guid.CreateVersion7());
+        Name = new(name);
+        CreateDate = DateTime.Now;
+    }
+
+    public static TodoList Create([Required, MaxLength(MAX_LENGTH_FOR_NAME), MinLength(MIN_LENGTH_FOR_NAME)] string name) => new(name);
+    public void Rename([Required, MaxLength(MAX_LENGTH_FOR_NAME), MinLength(MIN_LENGTH_FOR_NAME)] string name)
+    {
+        Name = new(name);
+        UpdateDate = DateTime.Now;
+    }
+    public bool IsValidForDelete() => this.All(t => t.IsComplete);
+}
