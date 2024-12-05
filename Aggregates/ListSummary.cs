11a3175ad36816971a72963461bb57f9c805616d
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-05 17:38:38 -0500

    added list and todo summary aggregates for display

---
+++ Aggregates/ListSummary.cs
@@ -0,0 +1,4 @@
+﻿
+namespace TodoList.Domain.Aggregates;
+
+public readonly record struct ListSummary(Guid ListId, string Name, int CountOfTodosNotComplete, DateTime CreateDate, DateTime? UpdateDate);
---
+++ Aggregates/TodoSummary.cs
@@ -0,0 +1,4 @@
+﻿
+namespace TodoList.Domain.Aggregates;
+
+public readonly record struct TodoSummary(Guid TodoId, Guid ListId, string Description, DateOnly? DueDate, bool IsImportant, bool IsComplete, DateTime CreateDate, DateTime? UpdateDate);
