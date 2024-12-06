ec512d7a4774a31a85f6efebbef0501d33dc2fb1
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-05 21:04:22 -0500

    added models to use for json serialization

---
+++ Models/Json/JsonTodo.cs
@@ -0,0 +1,14 @@
+﻿
+namespace TodoList.Domain.Models.Json;
+
+public class JsonTodo
+{
+    public Guid TodoId { get; set; }
+    public Guid TodoListId { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public DateTime? DueDate { get; set; }
+    public bool IsImportant { get; set; }
+    public bool IsComplete { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+}
---
+++ Models/Json/JsonTodoList.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TodoList.Domain.Models.Json;
+
+public class JsonTodoList
+{
+    public Guid TodoListId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreateDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+}
