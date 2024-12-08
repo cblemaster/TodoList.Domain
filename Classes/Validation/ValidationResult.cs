f6a60ed97f8c0d3a2f3ab8266aadf688158d1010
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-08 17:27:21 -0500

    added validation result class

---
+++ Classes/Validation/ValidationResult.cs
@@ -0,0 +1,8 @@
+﻿
+namespace TodoList.Domain.Classes.Validation;
+
+internal class ValidationResult
+{
+    internal required bool IsValid { get; set; }
+    internal required IEnumerable<string> ValidationErrors { get; set; }
+}
