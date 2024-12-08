de0a810e45cbd24249a5da2496e4e3b0c6392908
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-08 16:26:53 -0500

    deleted old content so as to start with a clean branch

--- Primitives/InvalidString.cs
+++
@@ -1,5 +0,0 @@
-﻿
-namespace TodoList.Domain.Primitives;
-
-internal record InvalidString(string Value, int MaxLength, bool IsRequired, bool IsAllowAllWhitespace, IEnumerable<string> ValidationErrors)
-    : ValidatableString(Value, MaxLength, IsRequired, IsAllowAllWhitespace);
