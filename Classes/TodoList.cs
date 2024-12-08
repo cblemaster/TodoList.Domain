b3ad6c7692183b8f215d10114c8e343651ae27b1
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-08 16:48:04 -0500

    added todo list entity

---
+++ Classes/TodoList.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TodoList.Domain.Classes;
+
+internal class TodoList : Entity<TodoList>
+{
+    internal Id<TodoList> _id;
+
+    internal IEnumerable<Todo> Todos { get; init; } = [];    
+    internal required override Id<TodoList> Id { get => _id; init => _id = value; }
+    internal required string ListName { get; init; }
+    internal required Dates Dates { get; init; }
+}
