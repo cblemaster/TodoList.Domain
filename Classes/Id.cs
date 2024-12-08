bd4c21e7e5d1b4fe86a2a07cd466df5b0549ffe2
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-08 16:46:40 -0500

    added id class that inherits from identifer

---
+++ Classes/Id.cs
@@ -0,0 +1,9 @@
+﻿
+namespace TodoList.Domain.Classes;
+
+internal class Id<T> : Identifier<T>
+{
+    private Guid _value;
+    
+    internal required override Guid Value { get => _value; init => _value = value; }
+}
