fa4e663bc738e0c69a54cb6a309563b0557268ea
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-08 16:48:16 -0500

    added todo entity

---
+++ Classes/Todo.cs
@@ -0,0 +1,14 @@
+﻿
+namespace TodoList.Domain.Classes;
+
+internal class Todo : Entity<Todo>
+{
+    private Id<Todo> _id;
+    
+    internal override required Id<Todo> Id { get => _id; init => _id = value; }
+    internal required string Description { get; init; }
+    internal DateOnly? DueDate { get; init; }
+    internal bool IsImportant { get; init; }
+    internal bool IsComplete { get; init; }
+    internal required Dates Dates { get; init; }
+}
