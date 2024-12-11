5ee19a2c2d3ba5ede203111b49721f968062000c
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-10 20:13:22 -0500

    added business logic to the entity classes

--- Entities/Todo.cs
+++ Entities/Todo.cs
@@ -3,4 +3,8 @@ using TodoList.Domain.ValueStructs;
 
 namespace TodoList.Domain.Entities;
 
-internal record Todo(TodoId Id, TodoListId ListId, TodoList List, Descriptor Description, DateOnly? DueDate, bool IsImportant, bool IsComplete, DateTimeStamps Dates);
+internal record Todo(TodoId Id, TodoListId ListId, TodoList List, Descriptor Description, DateOnly? DueDate, bool IsImportant, bool IsComplete, DateTimeStamps Dates)
+{
+    internal bool CanBeUpdated => !IsComplete;
+    internal bool CanBeDeleted => !IsImportant;
+}
--- Entities/TodoList.cs
+++ Entities/TodoList.cs
@@ -3,4 +3,7 @@ using TodoList.Domain.ValueStructs;
 
 namespace TodoList.Domain.Entities;
 
-internal record TodoList(TodoListId Id, Descriptor Name, ICollection<Todo> Todos, DateTimeStamps Dates);
+internal record TodoList(TodoListId Id, Descriptor Name, ICollection<Todo> Todos, DateTimeStamps Dates)
+{
+    internal bool CanBeDeleted = Todos.All(t => t.IsComplete);
+}
