33355156e28633bea1b286290fb63fe51075df13
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-11 21:05:38 -0500

    added aggregates

---
+++ Aggregates/SeeListWithTodos.cs
@@ -0,0 +1,8 @@
+﻿
+using TodoList.Domain.Entities;
+using TodoList.Domain.Primitives;
+using TodoList.Domain.ValueStructs;
+
+namespace TodoList.Domain.Aggregates;
+
+internal record SeeListWithTodos(TodoListId Id, Descriptor ListName, IEnumerable<Todo> Todos, DateTimeStamps Dates);
---
+++ Aggregates/SeeTodoWithList.cs
@@ -0,0 +1,7 @@
+﻿
+using TodoList.Domain.Primitives;
+using TodoList.Domain.ValueStructs;
+
+namespace TodoList.Domain.Aggregates;
+
+internal record SeeTodoWithList(TodoId Id, TodoListId ListId, Entities.TodoList List, Descriptor Description, DateOnly? DueDate, bool IsImportant, bool IsComplete, DateTimeStamps Dates);
